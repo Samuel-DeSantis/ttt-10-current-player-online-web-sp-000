fa5a7b3979776e9c03a83cf9de5d3b27d0c64688
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-24 02:41:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,11 +2,9 @@ def turn_count(board)
   turn = 0
   board.each do |item|
     (item === "X" || item === "O") ? turn += 1 : pass
-    =begin
-    if item === "X" || item === "O"
-      turn += 1
-    end
-    =end
+    #if item === "X" || item === "O"
+    #  turn += 1
+    #end
   end
   return turn
 end
